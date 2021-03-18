a57f6c3552b4aab1f2fea08a8edda9ff81be1287
lindsay-lang <lindsay.foly2020@campus-eni.fr>
2021-03-18 11:12:48 +0100

    maj

--- src/Controller/ConnectionController.php
+++ src/Controller/ConnectionController.php
@@ -19,18 +19,6 @@ class ConnectionController extends AbstractController
     }
 
 
-    /*public function listeSorties() : Response
-    {
-          return $this->render("templateListSorties.html.twig", [
-              $sorties = [
-                  'nom' => ['Philo', 'Origamie', 'Perle'],
-                  'date' => ['19/07/2018', '21/07/2018', '21/07/2018'],
-                  'cloture' => ['19/07/2018', '21/07/2018', '21/07/2018'],
-                  'inscrits/places' => ['8/8', '3/5', '2/12'],
-                  'Etat' => ['En cours', 'Fermé', 'Fermé'],
-                  'Inscrit' => ['X', ' ', 'X'],
-                  'Organisateur' => ['Spinoz A', 'Rémi S', 'Jojo56'],
-          ],]);
-}*/
+
 }
 
