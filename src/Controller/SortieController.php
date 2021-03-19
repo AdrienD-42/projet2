76caebb0907f77d2723ca359445005415aeda759
lindsay-lang <lindsay.foly2020@campus-eni.fr>
2021-03-19 20:36:42 +0100

    maj by Lfoly avec bugs :(

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,31 +2,60 @@
 
 namespace App\Controller;
 
+use App\Entity\Etat;
+use App\Entity\Site;
 use App\Entity\Sortie;
-use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class SortieController extends AbstractController
 {
     private $repository;
 
     /**
-     * @Route("/sortie", name="sortie")
+     * @Route("/liste_sorties", name="liste_sorties")
+     * @param EntityManagerInterface $em
      * @return Response
      */
-    public function Sortie(EntityManagerInterface $entityManager, SessionInterface $session): Response
+    public function listeSortie(EntityManagerInterface $em, Request $request)
     {
-        /** @var SortieRepository $sortieRepository */
-        $sortieRepository = $entityManager->getRepository(Sortie::class);
-        $sorties = $sortieRepository->findAll();
-        if ($sorties != null) {
-            return $this->render('sortie/templateListSorties.html.twig', [
-                'sorties' => $sorties,]);
-        }
-    }
-}
+// IDENTIFICATION DES PARAMETRES D'ENTREES
+// FILTRER LES SORTIES PAR ETAT
+        $idSite = $request->get('site');
+        $rechercheMot = $request->get('mot');
+        $dateDebut = $request->get('dateDebut');
+        $dateFin = $request->get('dateFin');
+        $organisateur = $request->get('checkbox_organisateur');
+        $sortiesInscrit = $request->get('checkbox_inscrit');
+        $sortiesNoInscrit = $request->get('checkbox_noInscrit');
+        $sortiesPassees = $request->get('checkbox_passee');
 
+        // RECUPERE TOUS LES SITES
+        $sites = $em->getRepository(Site::class)->findAll();
+
+        // RECUPERE TOUTES LES SORTIES FILTREES
+        $sorties = $em->getRepository(Sortie::class)->findBycriteres($idSite, $rechercheMot, $dateDebut, $dateFin,
+            $organisateur, $sortiesInscrit, $sortiesNoInscrit, $sortiesPassees);
+
+
+
+
+
+        return $this->render('sortie/templateListSorties.html.twig', [
+    //
+            'sites' => $sites,
+            'sorties' => $sorties,
+
+
+        ]);
+    }
+
+
+
+
+
+}
--- src/Repository/SortieRepository.php
+++ src/Repository/SortieRepository.php
@@ -2,9 +2,15 @@
 
 namespace App\Repository;
 
+
+use App\Entity\Participant;
 use App\Entity\Sortie;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+
+
 
 /**
  * @method Sortie|null find($id, $lockMode = null, $lockVersion = null)
@@ -19,32 +25,63 @@ class SortieRepository extends ServiceEntityRepository
         parent::__construct($registry, Sortie::class);
     }
 
-    // /**
-    //  * @return Sortie[] Returns an array of Sortie objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    public function findBycriteres($idSite = null, $rechercheMot = null, $dateDebut = null, $dateFin = null,
+                                     $organisateur = null, $sortiesInscrit = null, $sortiesNoInscrit = null, $sortiesPassees = null)
     {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('s.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
 
-    /*
-    public function findOneBySomeField($value): ?Sortie
-    {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
+        $qd = $this->createQueryBuilder('sortie')
+            ->join('sortie.site', 'site')
+            ->join('sortie.organisateur', 'organisateur')
+            ->join('sortie.etat', 'etat')
+            ->addSelect('organisateur')
+            ->addSelect('site')
+            ->addSelect('etat');
+
+        if ($idSite > 0) {
+            $qd->andWhere('site.id =: idSite')
+                ->setParameter('site', $idSite);
+
+        }
+        if ($rechercheMot != null) {
+            $qd->andWhere('sortie.nom =: rechercheMot')
+                ->setParameter('rechercheMot', '%' . $rechercheMot . '%');
+
+        }
+        if ($dateDebut != null) {
+            $qd->andWhere('sortie.date_debut =: dateDebut')
+                ->setParameter('dateDebut', '$dateDebut');
+
+        }
+        if ($dateFin != null) {
+            $qd->andWhere('sortie.date_cloture =: dateFin')
+                ->setParameter('dateFin', '$dateFin');
+
+        }
+        if ($organisateur != null) {
+            $organisateur = $this->getEntityManager()->getRepository(Participant::class)->find($organisateur);
+            $qd->andWhere('sortie.organisateur_id =: organisateur')
+                ->setParameter('checkbox_organisateur', '$organisateur');
+        }
+        if ($sortiesInscrit > 0) {
+            $inscrit = $this->getEntityManager()->getRepository(Participant::class)->find($sortiesInscrit);
+            $qd->andWhere('sortie.organisateur_id = :inscrit')
+                ->setParameter('checkbox_organisateur', '$inscrit');
+
+        }
+        if ($sortiesNoInscrit < 0) {
+            $noInscrit = $this->getEntityManager()->getRepository(Participant::class)->find($sortiesNoInscrit);
+            $qd->andWhere('sortie.organisateur_id =: user')
+                ->setParameter('checkbox_organisateur', '$noInscrit');
+        }
+        if ($sortiesPassees < 0) {
+            $qd->andWhere('etat.libelle = : checkbox_passee')
+                ->setParameter('checkbox_passee', '$checkbox_passee');
+
+
+        }
+
+
+        return $qd;
+
     }
-    */
 }
